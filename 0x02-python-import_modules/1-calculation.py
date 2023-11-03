38942f32739c8914c5451c9e71cdd0d4485101d7
Babuhatarii <ochegoharyon@gmail.com>
2023-11-03 04:08:03 -0700

    updated

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,8 +3,8 @@ a = 10
 b = 5
 
 if __name__ == "__main__":
-    from calculator_1 import add, subtract, multiply, divide
-    print("{:d} + {:d} = {:d}".format(a, b, add))
-    print("{:d} - {:d} = {:d}".format(a, b, subtract))
-    print("{:d} * {:d} = {:d}".format(a, b, multiply))
-    print("{:d} / {:d} = {:d}".format(a, b, divide))
+    from calculator_1 import add, sub, mul, div
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
--- 0x02-python-import_modules/calculator_1.py
+++ 0x02-python-import_modules/calculator_1.py
@@ -1,16 +1,51 @@
 #!/usr/bin/python3
-
 def add(a, b):
-    return a + b
+    """My addition function
 
-def subtract(a, b):
-    return a - b
+    Args:
+        a: first integer
+        b: second integer
 
-def multiply(a, b):
-    return a * b
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
 
-def divide(a, b):
-    if b != 0:
-        return a / b
-    else:
-        return "Division by zero is not allowed"
+
+def sub(a, b):
+    """My subtraction function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a - b
+    """
+    return (a - b)
+
+
+def mul(a, b):
+    """My multiplication function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a * b
+    """
+    return (a * b)
+
+
+def div(a, b):
+    """My division function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a / b
+    """
+    return int(a / b)
