933727020b2ec32d3afc2cb88105fc959b9b70d5
Babuhatarii <ochegoharyon@gmail.com>
2023-11-04 02:23:00 -0700

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
+import py_compile
+import sys
+
 if __name__ == "__main__":
+    # Compile the .pyc file into a .py file
+    py_compile.compile('hidden_4.pyc')
+
+    # Import the module
     import hidden_4
-    for s in dir(hidden_4):
-        if s[:2] != "__":
-            print("{:s}".format(s))
+
+    # Get all the names defined in the module
+    module_names = dir(hidden_4)
+
+    # Filter and print the names that do not start with double underscores
+    for name in sorted(module_names):
+        if not name.startswith('__'):
+            print(name)
