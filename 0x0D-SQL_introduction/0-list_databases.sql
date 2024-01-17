c44ea60e166f0abde16e833d3d145bceecd77f57
Babuhatarii <ochegoharyon@gmail.com>
2024-01-16 23:04:04 -0800

    0-list_databases.sql

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,7 +1,2 @@
-/*
- * Task: List Databases
- * Description: This script lists all databases on the MySQL server.
- */
-
-/* Display all databases */
+-- lists all databases of your MySQL server.
 SHOW DATABASES;
