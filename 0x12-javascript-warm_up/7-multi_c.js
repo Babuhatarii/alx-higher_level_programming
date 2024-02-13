e3f4ac6404d8cfeef6f5d76f4f37e72a7d2dc4aa
Babuhatarii <ochegoharyon@gmail.com>
2024-02-13 09:31:00 -0800

    7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,17 +1,6 @@
 #!/usr/bin/node
-
-const x = Math.floor(Number(process.argv[2]));
-
-if (isNaN(x)) {
-
-  console.log('Missing number of occurrences');
-
-} else {
-
-  for (let i = 0; i < x; i++) {
-
-    console.log('C is fun');
-
-  }
-
+const C = 'C is fun';
+const x = process.argv[2];
+for (let i = 0; i < x; i++) {
+  console.log(C);
 }
