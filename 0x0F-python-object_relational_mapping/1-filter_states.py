169be53fa7308c7de3e65726749b90f2b89ba2be
Babuhatarii <ochegoharyon@gmail.com>
2024-02-19 02:11:36 -0800

    1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,7 +7,8 @@ import sys
 import MySQLdb
 
 if __name__ == '__main__':
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], 
+                         db=sys.argv[3], port=3306)
 
     cur = db.cursor()
 
@@ -21,4 +22,3 @@ if __name__ == '__main__':
 
     for state in states:
         print(state)
-
