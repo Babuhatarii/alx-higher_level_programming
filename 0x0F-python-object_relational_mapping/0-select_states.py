33a29afbc88b6d4b7f389a3a431434b35b0117de
Babuhatarii <ochegoharyon@gmail.com>
2024-02-19 01:59:14 -0800

    mapping

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,40 +1,26 @@
 #!/usr/bin/python3
-"""
-Script to list all states from a MySQL database.
-"""
 
-import MySQLdb
+"""
+script that lists all states from the database hbtn_0e_0_usa:
+"""
 import sys
+import MySQLdb as db
 
 
-def list_states(username, password, database):
-    """
-    Connects to a MySQL database and lists all states.
+def connect_and_query() -> None:
 
-    Args:
-        username (str): MySQL username.
-        password (str): MySQL password.
-        database (str): Database name.
+    """Connects to the database and executes a query to select all states."""
+    try:
+        cnx = db.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+        cursor = cnx.cursor(cursorclass=db.cursors.Cursor)
+        cursor.execute('SELECT * FROM states ORDER BY `id` ASC;')
+        states = cursor.fetchall()
 
-    Returns:
-        None
-    """
-    connection = MySQLdb.connect(
-        host='localhost',
-        port=3306,
-        user=username,
-        passwd=password,
-        db=database
-    )
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-    states = cursor.fetchall()
-    for state in states:
-        print(state)
-    cursor.close()
-    connection.close()
+        for state in states:
+            print(state)
+    except Exception as e:
+        return (e)
 
 
 if __name__ == "__main__":
-    username, password, database = sys.argv[1:4]
-    list_states(username, password, database)
+    connect_and_query()
