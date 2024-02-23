85fa79d1c93cb509eedea66a52b71ca54a4578a8
Babuhatarii <ochegoharyon@gmail.com>
2024-02-23 09:11:59 -0800

    3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,15 +1,28 @@
 #!/usr/bin/python3
 """
-script that takes in arguments and displays all 
+script that takes in arguments and displays all
 values in the states table of hbtn_0e_0_usa
 """
 import sys
-import MySQLdb
-if __name__ == '__main__':
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
-                         db=sys.argv[3], port=3306)
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name = %s;", (sys.argv[4],))
-    states = cur.fetchall()
-    for state in states:
-        print(state)
+import MySQLdb as db
+
+
+def connect_and_query() -> None:
+
+    """Connect to the database and execute query"""
+    try:
+        cnx = db.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+        cursor = cnx.cursor(cursorclass=db.cursors.Cursor)
+        cursor.execute("SELECT * FROM states WHERE name = %s \
+                        ORDER BY id ASC;", (sys.argv[4], ))
+        states = cursor.fetchall()
+
+        for state in states:
+            if state[1] == sys.argv[4]:
+                print(state)
+    except Exception as e:
+        return (e)
+
+
+if __name__ == "__main__":
+    connect_and_query()
