c9cf1d5124ec0a99872f9f40501612beb0f04ca5
Babuhatarii <ochegoharyon@gmail.com>
2024-03-06 05:50:11 -0800

    HOLBERTON BACK-END INTERVIEW

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Script that lists 10 commits (from the most recent to oldest) of a repository
+by a specific user using the GitHub API.
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    repo_name = sys.argv[1]
+    owner_name = sys.argv[2]
+    url = f"https://api.github.com/repos/{owner_name}/{repo_name}/commits"
+
+    params = {
+        'per_page': 10,
+        'page': 1,
+        'author': owner_name
+    }
+
+    response = requests.get(url, params=params)
+    commits = response.json()
+
+    for commit in commits:
+        sha = commit['sha']
+        author_name = commit['commit']['author']['name']
+        print(f"{sha}: {author_name}")
